31b5152be311f1f59a1458911de2a658f08a6eb4
vo-nguye <vo-nguye@student.42.fr>
2021-01-06 09:32:31 +0100

    minimal working sample

--- main.c
+++ main.c
@@ -1,47 +1,11 @@
 #include "minishell.h"
 
-//int 	exec_cmd(char **cmd)
-//{
-//	// replace ENV, treat, redirection
-//	execve(cmd[0], cmd, 0);
-//	return (0);
-//}
-//
-//int 	execute_cmd_pipe(char ***cmds, int source)
-//{
-//	pid_t		pid;
-//	int 		pipe_fd[2];
-//
-//	if (cmds[0] == NULL && close(source) == 0)
-//		return 0;
-//	if (pipe(pipe_fd) < 0)
-//		return (-1);
-//	if (cmds[1] == NULL) // if it's the last command of the list => output = stdout
-//	{
-//		dup2(g_stdout_copy, 1);
-//		close(g_stdout_copy);
-//	}
-//	else
-//		dup2(pipe_fd[1], 1);
-//	dup2(source, 0);
-//	pid = fork();
-//	if (pid < 0)
-//		return (1);
-//	if (pid == CHILD_PROCESS && close(pipe_fd[0]) == 0)
-//		exec_cmd(cmds[0]);
-//	wait(0);
-//	close(pipe_fd[1]);
-//	close(source);
-//	return (execute_cmd_pipe(cmds + 1, pipe_fd[0]));
-//}
-
-
 int main()
 {
-	char *cm_1[] = {"/bin/cat", "minishell.c", NULL};
+	char *cm_1[] = {"/bin/cat", "main.c", NULL};
 	char *cm_2[] = {"/bin/grep", "if", NULL};
 	char *cm_3[] = {"/bin/grep", "=", NULL};
-	char *cm_4[] = {"/bin/grep", "0", NULL};
+	char *cm_4[] = {"/bin/grep", ";", NULL};
 	char **cmd_pipe[] = {cm_1, cm_2, cm_3, cm_4, NULL};
 
 	g_stdout_copy = dup(1);
